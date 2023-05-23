ba7605ee604238b0731e4058517ed623b51d3832
Michael Welle <michaelpwelle@gmail.com>
2023-05-22 19:59:10 -0700

    react setup

---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+export default function Header() {
+    return (
+        <>
+        <header>
+            <h1>Noted</h1>
+        </header>
+        <Navbar />
+    </>
+    )
+}
