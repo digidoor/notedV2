288e7b39d1eb572b2924b4bd45c5d7a67c37fb84
Bri Dray <stanggurl02@gmail.com>
2023-05-26 13:17:33 -0700

    started resizing input bars and buttons

--- client/src/pages/Nutrition.js
+++ client/src/pages/Nutrition.js
@@ -33,6 +33,24 @@ const styles = {
 
   nutContainer: {
     marginTop: '30px',
+  },
+
+  // Recipe column
+  searchBar: {
+    width: '80%'
+  },
+
+  searchBtn: {
+    width: '15%'
+  },
+
+  // meal plan column
+  addBtn: {
+    width: '50%',
+  },
+
+  clearBtn: {
+    width: '50%',
   }
 
 }
@@ -44,8 +62,8 @@ export default function Nutrition() {
       <div className="column-left" style={styles.columnLeft}>
         <div className="card-one recipeUrl" id="recipe" style={styles.cardOne}>
           <h4>Recipes</h4>
-          <input id="userSearch" type="search" placeholder="Search Recipe"></input>
-          <button className="btn btn-danger" id="fetch-button">Search</button>
+          <input id="userSearch" type="search" placeholder="Search Recipe" style={styles.searchBar}></input>
+          <button className="btn btn-danger" id="fetch-button" style={styles.searchBtn}>Search</button>
           <div id="results"></div>
         </div>
       </div>
@@ -55,10 +73,10 @@ export default function Nutrition() {
           <div className="card-two" style={styles.cardTwo}>
             <h4>Meal Plan</h4>
             <input className="add" id="addRecipe" type="text" placeholder="+ your recipe URL here"></input>
-            <button className="add btn btn-danger" id="add">Add</button>
+            <button className="add btn btn-danger" id="add" style={styles.addBtn}>Add</button>
 
             <ul id="savedList"></ul>
-            <button className="btn btn-danger" id="clear-button">clear</button>
+            <button className="btn btn-danger" id="clear-button" style={styles.clearBtn}>clear</button>
 
           </div>
         </div>
