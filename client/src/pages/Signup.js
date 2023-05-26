710150cbce2efe980310d0c8e0ccfdd14ce08ac3
Bri Dray <stanggurl02@gmail.com>
2023-05-26 10:02:49 -0700

    fixed spacing for the login anchor and added game between container and header.

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -2,10 +2,20 @@ import React from 'react';
 import Welcome from '../components/Welcome';
 import SignupForm from '../components/SignupForm';
 
+const styles = {
+    // login here anchor
+loginAnch: {
+marginTop: '15px',
+},
+signUpContainer: {
+    marginTop: '30px',
+}
+}
+
 const Signup = () => {
 
     return (
-        <div className="container text-center">
+        <div className="container text-center" style={styles.signUpContainer}>
             <div className="row">
                 <div className="col">
                     <Welcome/>
@@ -15,7 +25,7 @@ const Signup = () => {
                 </div>
             </div>
             <a href='/login'>
-                <h3>Login</h3>
+                <h3 style={styles.loginAnch}>Login Here</h3>
             </a>
         </div>
     );
