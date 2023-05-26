0cd72bc84f550923b5912200203238ee06aeda06
Michael Welle <michaelpwelle@gmail.com>
2023-05-25 19:00:50 -0700

    client router and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 // import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const Signup = () => {
     const [formState, setFormState] = useState({
@@ -27,7 +28,7 @@ const Signup = () => {
         console.log(formState);
 
         try {
-            const { data } = await addUser({
+            const { data } = await ADD_USER({
                 variables: { ...formState },
             });
 
@@ -43,6 +44,19 @@ const Signup = () => {
             <div>
                 <h2>Sign-Up</h2>
                 <form onSubmit={handleFormSubmit}>
+                <div class="form-floating mb-3">
+                        <input
+                            className="form-control"
+                            id="floatingInput"
+                            placeholder="username"
+                            name="username"
+                            type="username"
+                            value={formState.username}
+                            onChange={handleChange}
+
+                        />
+                        <label for="floatingInput">Username</label>
+                    </div>
                     <div class="form-floating mb-3">
                         <input
                             className="form-control"
@@ -67,7 +81,7 @@ const Signup = () => {
                         <label for="floatingPassword">Password</label>
                     </div>
                     <div class="d-grid gap-2 col-6 mx-auto">
-                        <button className="btn btn-primary" type="button">Sign-Up</button>
+                        <button className="btn btn-primary" type="submit" >Sign-Up</button>
                     </div>
                 </form>
                 <img src="./stickynotelarge - Copy.png" alt="sticky note image" width="500px" height="500px" />
