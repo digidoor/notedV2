d71dab22cedb0e98229d9be8308e8d54ae1ed7ba
Jonah Lewis <jonahslewis96@gmail.com>
2023-05-26 09:17:52 -0700

    merge

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -15,41 +15,6 @@ const Login = () => {
                     <Welcome/>
                 </div>
                 <div className="col">
-                    <div>
-                        <h2>Sign-In</h2>
-                        <form onSubmit={handleFormSubmit}>
-                            <div className="form-floating mb-3">
-                                <input
-                                    className="form-control"
-                                    id="floatingInput"
-                                    placeholder="name@example.com"
-                                    name="email"
-                                    type="email"
-                                    value={formState.email}
-                                    onChange={handleChange}
-
-                                />
-                                <label for="floatingInput">Email address</label>
-                            </div>
-                            <div className="form-floating">
-                                <input
-                                    className="form-control"
-                                    id="floatingPassword"
-                                    placeholder="Password"
-                                    name="password"
-                                    type="password"
-                                    value={formState.password}
-                                    onChange={handleChange}
-                                />
-                                <label for="floatingPassword">Password</label>
-                            </div>
-                            <div className="d-grid gap-2 col-6 mx-auto">
-                                <button className="btn btn-primary" type="submit">Sign-In</button>
-                            </div>
-                        </form>
-                        <img src="./stickynotelarge - Copy.png" alt="sticky note image" width="500px" height="500px" />
-                    </div>
-                <div className="col">
                     <LoginForm/>
                 </div>
             </div>
