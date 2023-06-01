a8bceca17a9d0ef7e4b91f982b064f8b00a9202c
Bri Dray <stanggurl02@gmail.com>
2023-06-01 12:03:07 -0700

    footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,17 +1,86 @@
-import React from "react"
+import React from 'react';
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBCol,
+  MDBRow,
+  MDBIcon,
+  MDBBtn
+} from 'mdb-react-ui-kit';
 
+export default function Footer() {
+  return (
+    <MDBFooter className='bg-light text-center text-white'>
+      <MDBContainer className='p-4 pb-0'>
+        <section className='mb-4'>
+          <MDBBtn
+            floating
+            className='m-1'
+            style={{ backgroundColor: '#feedaa', boxShadow: '4px 2px 0 0 grey' }}
+            href='#!'
+            role='button'
+          >
+            <MDBIcon fab icon='facebook-f' />
+          </MDBBtn>
 
+          <MDBBtn
+            floating
+            className='m-1'
+            style={{ backgroundColor: '#55acee' }}
+            href='#!'
+            role='button'
+          >
+            <MDBIcon fab icon='twitter' />
+          </MDBBtn>
 
-const Note = () => {
+          <MDBBtn
+            floating
+            className='m-1'
+            style={{ backgroundColor: '#dd4b39' }}
+            href='#!'
+            role='button'
+          >
+            <MDBIcon fab icon='google' />
+          </MDBBtn>
+          <MDBBtn
+            floating
+            className='m-1'
+            style={{ backgroundColor: '#ac2bac', color: 'black', }}
+            href='#!'
+            role='button'
+          >
+            <MDBIcon fab icon='instagram' />
+          </MDBBtn>
 
+          <MDBBtn
+            floating
+            className='m-1'
+            style={{ backgroundColor: '#0082ca' }}
+            href='#!'
+            role='button'
+          >
+            <MDBIcon fab icon='linkedin-in' />
+          </MDBBtn>
 
-
-    return (
-        <>
-        <div>
-            
-        </div>
-        </>
-    )
+          <MDBBtn
+            floating
+            className='m-1'
+            style={{ backgroundColor: '#333333' }}
+            href='#!'
+            role='button'
+          >
+            <MDBIcon fab icon='github' />
+          </MDBBtn>
+        </section>
+      </MDBContainer>
+      <div className='text-center p-3' style={{ backgroundColor: '#bbe8d9' }}>
+        © 2020 Copyright:
+        <a className='text-white'>
+          Noted
+        </a>
+      </div>
+    </MDBFooter>
+  );
 }
- export default Note
+
+//  export default Footer
