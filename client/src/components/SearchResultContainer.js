50ef6cf2d49764a66e90598d320c6734163a448a
Jonah Lewis <jonahslewis96@gmail.com>
2023-06-01 18:30:08 -0700

    got search results to be passed through with image and clickable title as link on Nutrition.js page

--- client/src/components/SearchResultContainer.js
+++ client/src/components/SearchResultContainer.js
@@ -5,32 +5,15 @@ import RecipeList from './RecipeList';
 import search from '../utils/nutritionApi';
 
 const SearchResultContainer = (props) => {
-  // Declare a new state variable, "results"
-  const [results, setResults] = useState({});
-
-  // Method to get search results and set state
-  const searchRecipe = async (query) => {
-    try {
-      const response = await search(query);
-      console.log(response)
-      setResults(response.data.results);
-    } catch (err) {
-      console.log(err)
-    }
-
-  };
 
   // We want to run this method when the component first loads so that we have images of kittens to display
   // The second argument is the dependency array. This means that this method will only run when the component first loads
-  useEffect(() => {
-    searchRecipe(props.query);
-  }, []);
-
+ 
 
   return (
     <div>
       {/* Pass our results to the ResultsList component to map over */}
-      {results ? <RecipeList results={results} /> : null}
+      {props.query ? <RecipeList results={props.query} /> : null}
     </div>
   );
 };
