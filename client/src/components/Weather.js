3f6e3d137509afb4c3a893e4353df1ce7cbb88a6
Bri Dray <stanggurl02@gmail.com>
2023-06-01 21:32:07 -0700

    fixed class names on the weather html

--- client/src/components/Weather.js
+++ client/src/components/Weather.js
@@ -17,131 +17,131 @@ const Weather = () => {
     return (
         <>
             <h1 class="weather">Weather</h1>
-            <div class="card-group">
-                <div class="card">
-                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp" class="card-img-top"
+            <div className="card-group">
+                <div className="card">
+                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp" className="card-img-top"
                         alt="Hollywood Sign on The Hill" />
-                    <div class="card-body">
-                        <h5 class="card-title">Date from API</h5>
+                    <div className="card-body">
+                        <h5 className="card-title">Date from API</h5>
                         <h6>Temperature</h6>
                         <h7>Current:</h7>
-                        <p class="card-text">CurrTemp</p>
+                        <p className="card-text">CurrTemp</p>
                         <h7>High</h7>
-                        <p class="card-text">HighTemp</p>
+                        <p className="card-text">HighTemp</p>
                         <h7>Low</h7>
-                        <p class="card-text">LowTemp</p>
+                        <p className="card-text">LowTemp</p>
                         <h6>Humidity</h6>
-                        <p class="card-text">HumidRes</p>
+                        <p className="card-text">HumidRes</p>
                         <h6>Wind</h6>
-                        <p class="card-text">WindRes</p>
+                        <p className="card-text">WindRes</p>
                     </div>
                 </div>
-                <div class="card">
-                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp" class="card-img-top"
+                <div className="card">
+                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp" className="card-img-top"
                         alt="Palm Springs Road" />
-                    <div class="card-body">
+                    <div className="card-body">
+                        <h5 className="card-title">Date from API</h5>
+                        <h6>Temperature</h6>
+                        <h7>Current</h7>
+                        <p className="card-text">CurrentTemp</p>
+                        <h7>High</h7>
+                        <p className="card-text">HighTemp</p>
+                        <h7>Low</h7>
+                        <p className="card-text">LowTemp</p>
+                        <h6>Humidity</h6>
+                        <p className="card-text">HumidRes</p>
+                        <h6>Wind</h6>
+                        <p className="card-text">WindRes</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp" className="card-img-top"
+                        alt="Palm Springs Road" />
+                    <div className="card-body">
+                        <h5 className="card-title">Date from API</h5>
+                        <h6>Temperature</h6>
+                        <h7>Current</h7>
+                        <p className="card-text">CurrentTemp</p>
+                        <h7>High</h7>
+                        <p className="card-text">HighTemp</p>
+                        <h7>Low</h7>
+                        <p className="card-text">LowTemp</p>
+                        <h6>Humidity</h6>
+                        <p className="card-text">HumidRes</p>
+                        <h6>Wind</h6>
+                        <p className="card-text">WindRes</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp" className="card-img-top"
+                        alt="Palm Springs Road" />
+                    <div className="card-body">
+                        <h5 className="card-title">Date from API</h5>
+                        <h6>Temperature</h6>
+                        <h7>Current</h7>
+                        <p className="card-text">CurrentTemp</p>
+                        <h7>High</h7>
+                        <p className="card-text">HighTemp</p>
+                        <h7>Low</h7>
+                        <p className="card-text">LowTemp</p>
+                        <h6>Humidity</h6>
+                        <p className="card-text">HumidRes</p>
+                        <h6>Wind</h6>
+                        <p className="card-text">WindRes</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp" className="card-img-top"
+                        alt="Palm Springs Road" />
+                    <div className="card-body">
+                        <h5 className="card-title">Date from API</h5>
+                        <h6>Temperature</h6>
+                        <h7>Current</h7>
+                        <p className="card-text">CurrentTemp</p>
+                        <h7>High</h7>
+                        <p className="card-text">HighTemp</p>
+                        <h7>Low</h7>
+                        <p className="card-text">LowTemp</p>
+                        <h6>Humidity</h6>
+                        <p className="card-text">HumidRes</p>
+                        <h6>Wind</h6>
+                        <p className="card-text">WindRes</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp" className="card-img-top"
+                        alt="Palm Springs Road" />
+                    <div className="card-body">
                         <h5 class="card-title">Date from API</h5>
                         <h6>Temperature</h6>
                         <h7>Current</h7>
-                        <p class="card-text">CurrentTemp</p>
+                        <p className="card-text">CurrentTemp</p>
                         <h7>High</h7>
-                        <p class="card-text">HighTemp</p>
+                        <p className="card-text">HighTemp</p>
                         <h7>Low</h7>
-                        <p class="card-text">LowTemp</p>
+                        <p className="card-text">LowTemp</p>
                         <h6>Humidity</h6>
-                        <p class="card-text">HumidRes</p>
+                        <p className="card-text">HumidRes</p>
                         <h6>Wind</h6>
-                        <p class="card-text">WindRes</p>
+                        <p className="card-text">WindRes</p>
                     </div>
                 </div>
-                <div class="card">
-                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp" class="card-img-top"
-                        alt="Palm Springs Road" />
-                    <div class="card-body">
-                        <h5 class="card-title">Date from API</h5>
-                        <h6>Temperature</h6>
-                        <h7>Current</h7>
-                        <p class="card-text">CurrentTemp</p>
-                        <h7>High</h7>
-                        <p class="card-text">HighTemp</p>
-                        <h7>Low</h7>
-                        <p class="card-text">LowTemp</p>
-                        <h6>Humidity</h6>
-                        <p class="card-text">HumidRes</p>
-                        <h6>Wind</h6>
-                        <p class="card-text">WindRes</p>
-                    </div>
-                </div>
-                <div class="card">
-                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp" class="card-img-top"
-                        alt="Palm Springs Road" />
-                    <div class="card-body">
-                        <h5 class="card-title">Date from API</h5>
-                        <h6>Temperature</h6>
-                        <h7>Current</h7>
-                        <p class="card-text">CurrentTemp</p>
-                        <h7>High</h7>
-                        <p class="card-text">HighTemp</p>
-                        <h7>Low</h7>
-                        <p class="card-text">LowTemp</p>
-                        <h6>Humidity</h6>
-                        <p class="card-text">HumidRes</p>
-                        <h6>Wind</h6>
-                        <p class="card-text">WindRes</p>
-                    </div>
-                </div>
-                <div class="card">
-                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp" class="card-img-top"
-                        alt="Palm Springs Road" />
-                    <div class="card-body">
-                        <h5 class="card-title">Date from API</h5>
-                        <h6>Temperature</h6>
-                        <h7>Current</h7>
-                        <p class="card-text">CurrentTemp</p>
-                        <h7>High</h7>
-                        <p class="card-text">HighTemp</p>
-                        <h7>Low</h7>
-                        <p class="card-text">LowTemp</p>
-                        <h6>Humidity</h6>
-                        <p class="card-text">HumidRes</p>
-                        <h6>Wind</h6>
-                        <p class="card-text">WindRes</p>
-                    </div>
-                </div>
-                <div class="card">
-                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/042.webp" class="card-img-top"
-                        alt="Palm Springs Road" />
-                    <div class="card-body">
-                        <h5 class="card-title">Date from API</h5>
-                        <h6>Temperature</h6>
-                        <h7>Current</h7>
-                        <p class="card-text">CurrentTemp</p>
-                        <h7>High</h7>
-                        <p class="card-text">HighTemp</p>
-                        <h7>Low</h7>
-                        <p class="card-text">LowTemp</p>
-                        <h6>Humidity</h6>
-                        <p class="card-text">HumidRes</p>
-                        <h6>Wind</h6>
-                        <p class="card-text">WindRes</p>
-                    </div>
-                </div>
-                <div class="card">
-                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp" class="card-img-top"
+                <div className="card">
+                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/043.webp" className="card-img-top"
                         alt="Los Angeles Skyscrapers" />
-                    <div class="card-body">
-                        <h5 class="card-title">Date from API</h5>
+                    <div className="card-body">
+                        <h5 className="card-title">Date from API</h5>
                         <h6>Temperature</h6>
                         <h7>Current</h7>
-                        <p class="card-text">CurrentTemp</p>
+                        <p className="card-text">CurrentTemp</p>
                         <h7>High</h7>
-                        <p class="card-text">HighTemp</p>
+                        <p className="card-text">HighTemp</p>
                         <h7>Low</h7>
-                        <p class="card-text">LowTemp</p>
+                        <p className="card-text">LowTemp</p>
                         <h6>Humidity</h6>
-                        <p class="card-text">HumidRes</p>
+                        <p className="card-text">HumidRes</p>
                         <h6>Wind</h6>
-                        <p class="card-text">WindRes</p>
+                        <p className="card-text">WindRes</p>
                     </div>
                 </div>
             </div>
