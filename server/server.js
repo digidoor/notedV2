7335b02080baec47c942d5955b21ad946f7074cd
Michael Welle <michaelpwelle@gmail.com>
2023-06-04 21:11:19 -0700

    heroku

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
-
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
